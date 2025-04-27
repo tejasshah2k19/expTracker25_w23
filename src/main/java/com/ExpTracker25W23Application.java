1c814d827267f7e7aa71d0135ddebbc479deed42
Tejas Shah <tejasshah2k19@gmail.com>
2025-04-27 16:49:39 +0530

    init

---
+++ src/main/java/com/ExpTracker25W23Application.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpTracker25W23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpTracker25W23Application.class, args);
+	}
+
+}
